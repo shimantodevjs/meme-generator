23ac1687f69657b71bb751f714d9e3d98ae31cf6
unknown <shimantodevjs@gmail.com>
2023-04-06 11:41:35 +0300

    final commit

--- src/assets/meme.jsx
+++ src/assets/meme.jsx
@@ -13,14 +13,19 @@ function Meme(){
         randomImage:"https://static1.pocketlintimages.com/wordpress/wp-content/uploads/140427-apps-news-the-best-stupidest-and-most-famous-internet-memes-around-image1-lm1toysfy4.jpg?q=50&fit=contain&w=1140&h=&dpr=1.5"
     })
 
-    function handleChange(){
+    function handleChange(event){
+           const{name,value}=event.target
 
+           setMeme(prevMeme=>({
+            ...prevMeme,
+            [name]:value
+           }))
     }
-
     
     // Функция для генерации случайного изображения мема из массива данных.
     function getRandomMemeImage(event){
             event.preventDefault()
+
             const randomNumber= Math.floor(Math.random()* memeData.length)
             const photos= memeData[randomNumber].photo
             setMeme(prevMeme=>({
@@ -38,13 +43,17 @@ function Meme(){
              className="input-txt-1"
              placeholder="Top Text"
              name="topText"
-
+             value={meme.topText}
+             onChange={handleChange}
              />
              
              <input 
              type="text" 
              className="input-txt-2" 
              placeholder="Bottom Text"
+             name="bottomText"
+             value={meme.bottomText}
+             onChange={handleChange}
              />
              </div>
 
@@ -52,6 +61,8 @@ function Meme(){
 
              <div className="meme__img">
                    <img src={meme.randomImage} alt="" />
+                   <h1 className="meme__text top">{meme.topText}</h1>
+                   <h1 className="meme__text bottom">{meme.bottomText}</h1>
             </div>  
             </form>
             
